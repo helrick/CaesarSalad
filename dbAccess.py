9aa22a6a018a8506a8be0a981e2f529deb2fd7b8
Hannah Greer <hannahgreer@rogers.com>
2018-02-03 14:55:28 -0500

    Added db access functions

---
+++ dbAccess.py
@@ -0,0 +1,41 @@
+# Find entries from sqlite db in python
+
+import sqlite3
+
+def queryTableByKeywords(conn, keyword1, keyword2, table):
+	cur = conn.cursor()
+	query = "SELECT A.argument FROM (select *  from " + table + " where argument like '%" + keyword1 + "%') A INNER JOIN (SELECT  * from " + table + " where argument like '%" + keyword2 + "%') B on A.id = B.id;"
+	cur.execute(query)
+	rows = cur.fetchall()
+	results = []
+	for row in rows:
+		row = row[0].encode('utf-8')
+		print row
+		results.append(row)
+	return results
+
+def queryTableByKeyword(conn, keyword, table):
+	cur = conn.cursor()
+	query = "select argument from " + table + " where argument like '%" + keyword + "%'"
+	cur.execute(query)
+	rows = cur.fetchall()
+	results = []
+	for row in rows:
+		row = row[0].encode('utf-8')
+		results.append(row)
+	return results
+
+def create_connection(db_file):
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+ 
+    return None
+
+def main():
+	conn = create_connection("ShouldWeGoVegan.db")
+	queryTableByKeywords(conn, "delicious", "products", "Against")
+
+main()
