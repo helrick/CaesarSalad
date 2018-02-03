0697b7241cf8a52bd5aa11c766358917a649bb5c
christinayan <christina.yan@queensu.ca>
2018-02-03 14:25:57 -0500

    added result ranking

--- indico.py
+++ indico.py
@@ -1,8 +1,4 @@
-# coding=utf-8
-from __future__ import print_function
-import json
-from os.path import join, dirname
-from watson_developer_cloud import TextToSpeechV1
+import operator
 
 import indicoio
 indicoio.config.api_key = '3ccad861571b4040c5312e55d9884722'
@@ -11,9 +7,23 @@ indicoio.config.api_key = '3ccad861571b4040c5312e55d9884722'
 def keyword(s):
     result = indicoio.keywords(s)
     keys = [key.encode("utf-8") for key in result]
-    relevancy = relevant(s, keys)
+    #print(keys)
+    return relevant(s, keys)
+
+def rankResults(results, key1, key2):
+    rank1 = indicoio.relevance(results, key1)
+    rank2 = indicoio.relevance(results, key2)
+    total = {}
+    for key in rank1:
+        total[key] = rank1[key] + rank2[key]
+    return dict(sorted(total.iteritems(), key=operator.itemgetter(1), reverse=True)[:1])
 
 def relevant(s, keys):
     scores = indicoio.relevance(s, keys)
     result = dict(zip(keys, scores))
+    result = dict(sorted(result.iteritems(), key=operator.itemgetter(1), reverse=True)[:2])
+    print(result)
     return result
+    # dbSearch(relevancy[0], relevancy[1])
+
+print(keyword("I think capitalism is sustainable because it supports the economy"))
